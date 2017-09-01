caab1db42b39dff4b8c0a0f30b0e63ff55a6c983
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 14:53:04 +0530

    Update as3.c

--- as3.c
+++ as3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int aq;
+char aq;
 printf("enter the number");
-scanf("%d",aq);
+scanf("%c",aq);
 if(aq=='a'||aq=='e'||aq=='i'||aq=='o'||aq=='u'||aq=='A'||aq=='E'||aq=='I'||aq=='O'||aq==U)
 {
-printf("the given value is vowel %d"aq);
+printf("the given value is vowel %c"aq);
 }
 else
 {
-printf("the given value is consonent%d"aq);
+printf("the given value is consonent%c"aq);
 }getch();
 }
