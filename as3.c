65fc4969440a771e604dc476f39b6b7db8cec519
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 14:45:37 +0530

    Create as3.c

---
+++ as3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int aq;
+printf("enter the number");
+scanf("%d",aq);
+if(aq=='a'||aq=='e'||aq=='i'||aq=='o'||aq=='u'||aq=='A'||aq=='E'||aq=='I'||aq=='O'||aq==U)
+{
+printf("the given value is vowel %d"aq);
+}
+else
+{
+printf("the given value is consonent%d"aq);
+}getch();
+}
