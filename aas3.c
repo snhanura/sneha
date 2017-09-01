7c062e12ae7db80d7fed3e5ccba394baa3122547
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 15:02:29 +0530

    Create aas3.c

---
+++ aas3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void main()
+ {
+char aq;
+ printf("enter the number");
+scanf("%c",aq);
+ if(aq=='a'||aq=='e'||aq=='i'||aq=='o'||aq=='u'||aq=='A'||aq=='E'||aq=='I'||aq=='O'||aq==U)
+ {
+printf("the given value is vowel %c"aq);
+ }
+ else
+ {
+printf("the given value is consonent%c"aq);
+ }getch();
+ }
