e21d6be8586be9b9ad4ae6cbc5154c0da56a6640
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 15:16:05 +0530

    Create as4.c

---
+++ as4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char ap;
+printf("enter the value");
+scanf("%c",ap);
+if((ap>'a'&&ap<'z')||(ap>'A'&&ap<'Z'))
+{
+printf("the given value is alphabet")
+}
+else
+{
+printf("the given value is not an alphabet");
+}getch(0;
+}
