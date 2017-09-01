1882da0a50f5dd3606b941afe25db3f39e4a9db8
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-01 14:15:48 +0530

    Create as2.c

---
+++ as2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",a);
+if(a%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}getch();
+}
