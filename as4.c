a19cb781890c7aebef1b4c45c3b388141b1e7052
paridhi1314 <61877547+paridhi1314@users.noreply.github.com>
2020-10-08 02:00:15 +0530

    Update as4.c

--- as4.c
+++ as4.c
@@ -6,10 +6,10 @@ printf("enter the value");
 scanf("%c",ap);
 if((ap>'a'&&ap<'z')||(ap>'A'&&ap<'Z'))
 {
-printf("the given value is alphabet")
+printf("the given value is alphabet");
 }
 else
 {
 printf("the given value is not an alphabet");
-}getch(0;
+}getch();
 }
